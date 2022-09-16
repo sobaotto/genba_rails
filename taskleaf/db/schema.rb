738f2c9e515dbd733ce470ad26c768ecb9d6a79f
sobao <ryota.kono@mazrica.com>
2022-09-16 09:19:22 +0900

    4-5-2 && 4-5-3

--- taskleaf/db/schema.rb
+++ taskleaf/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_15_120619) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_15_233752) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_15_120619) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "email", null: false
+    t.string "password_digest", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
